b9319834895d69813a7e1d7968abbfd547d81f35
Reviara <ajg.vanleeuwen@student.avans.nl>
2014-01-16 20:55:16 +0100

    Fixed client issue

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -35,20 +35,24 @@ public class FileHandler {
     }
 
     public void saveFile(InputStream sis, String fileName, int fileSize) throws ClientException {
-        File file = new File(Constants.CLIENT_PATH, (fileName));
-
-        if (!file.exists() || !file.isFile())
-            throw new ClientException("Cannot find the file specified.");
+        File file = new File(Constants.CLIENT_PATH + fileName);
+        file.getParentFile().mkdirs();
 
         try {
-            FileOutputStream fos = new FileOutputStream(file);
-            int buffSize = Math.min(Constants.BUFFER_SIZE, fileSize);
-            byte[] buffer = new byte[buffSize];
+            int remainingSize = fileSize;
+            while(remainingSize != 0)
+            {
+                FileOutputStream fos = new FileOutputStream(file);
+                int buffSize = Math.min(Constants.BUFFER_SIZE, remainingSize);
+                byte[] buffer = new byte[buffSize];
 
-            while (sis.read(buffer) != -1)
+                sis.read(buffer);
                 fos.write(buffer);
+
+                remainingSize -= buffSize;
+            }
         } catch (IOException e) {
-            throw new ClientException("Error while reading the file.");
+            throw new ClientException("Error while saving the file.");
         }
     }
 
