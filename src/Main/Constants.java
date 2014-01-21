54285b6ec1ede09da4ed98055eaf6fe2b3f9b3e4
Reviara <ajg.vanleeuwen@student.avans.nl>
2014-01-21 11:55:03 +0100

    Repaired constants file

--- src/Main/Constants.java
+++ src/Main/Constants.java
@@ -4,12 +4,15 @@ public class Constants {
     public static final int BUFFER_SIZE = 1337;
     public static final int MAX_READLINE_SIZE = 1337;
 
-    public static final String CLIENT_PATH = "/tmp/testclient/";
+    public static final String CLIENT_PATH;
     public static final String PROTOCOL = "SUPERSECRETPROTOCOL-1.0";
 
     static
     {
         String OS = System.getProperty("os.name", "generic").toLowerCase();
-
+        if(OS.equals("windows 8"))
+            CLIENT_PATH = "C:/Temp/Client/";
+        else
+            CLIENT_PATH = "/tmp/testclient/";
     }
 }
